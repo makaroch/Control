b2f6591c0f09f81983f1f071c1c7353bec0001ee
Alex <da.ek.ned@yandex.ru>
2022-11-08 13:53:13 +0300

    Задача решина для зарание заданного массива

--- Program.cs
+++ Program.cs
@@ -1,25 +1,33 @@
-﻿
-void Tack(){
-    string[] inputArray = {"123","1234","2345","12"};
+﻿string[] inputArray = {"123","1234","2345","12"};
 
-    string[] outputArray = new string[0]; 
+string[] outputArray = new string[0]; 
     
-    int length = inputArray.Length;
+int length = inputArray.Length;
 
+ for (int i = 0; i < length; i++)
+{
+    if (inputArray[i].Length <= 3)
+        AddNewElement(inputArray[i]);
+}
+
+LookArray(outputArray);
+
+
+
+void AddNewElement(string item){
+
+    int length = outputArray.Length; 
+   
+    Array.Resize(ref outputArray, length+1);
+    
+    outputArray[length] = item;
+  
+}
+
+void LookArray(string[] array){
+    int length = array.Length;
     for (int i = 0; i < length; i++)
     {
-        if (inputArray[i].Length <= 3)
-            AddNewElement(inputArray[i], outputArray);
+        Console.Write(array[i] + ", ");
     }
 }
-Tack();
-
-
-void AddNewElement(string item, string[] array){
-
-    int length = array.Length;
-   
-
-}
-
-
