e3ffb43322e25a448b1795e8a04bd07d3120a5e9
Alex <da.ek.ned@yandex.ru>
2022-11-08 13:37:03 +0300

    создаю метод добавления элемента в массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void Tack(){
+    string[] inputArray = {"123","1234","2345","12"};
+
+    string[] outputArray = new string[0]; 
+    
+    int length = inputArray.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+            AddNewElement(inputArray[i], outputArray);
+    }
+}
+Tack();
+
+
+void AddNewElement(string item, string[] array){
+
+    int length = array.Length;
+   
+
+}
+
+
